c230b68e2375ec5f8adae867b2858bc83c81b579
Jerry William <wilguepardo@gmail.com>
2021-12-22 01:14:46 -0300

    Projeto Internet Store - 14ª modificação (Inserindo a nova página serviços.html, com section indicativo, servicos e footer, com imagens e ajustes na responsividade e na formatação)

---
+++ js/dialog.js
@@ -0,0 +1,97 @@
+// Variáveis
+var btnAbreDialog1 = document.querySelector('#abrir-dialog1');
+var btnAbreDialog2 = document.querySelector('#abrir-dialog2');
+var btnAbreDialog3 = document.querySelector('#abrir-dialog3');
+var btnAbreDialog4 = document.querySelector('#abrir-dialog4');
+var btnAbreDialog5 = document.querySelector('#abrir-dialog5');
+var btnAbreDialog6 = document.querySelector('#abrir-dialog6');
+var dialog = document.querySelector('.dialog');
+
+
+// Abrir dialog 1
+btnAbreDialog1.addEventListener('click', function() {
+  dialog.classList.add('dialog--aberto');
+});
+
+function fechandoDialog() {
+  document.activeElement.blur();
+  dialog.classList.remove('dialog--aberto');     
+}
+
+// Listeners
+document.querySelector('.dialog-fechar').addEventListener('click', fechandoDialog);
+
+
+
+// Abrir dialog 2
+btnAbreDialog2.addEventListener('click', function() {
+  dialog.classList.add('dialog--aberto');
+});
+
+function fechandoDialog() {
+  document.activeElement.blur();
+  dialog.classList.remove('dialog--aberto');     
+}
+
+// Listeners
+document.querySelector('.dialog-fechar').addEventListener('click', fechandoDialog);
+
+
+
+// Abrir dialog 3
+btnAbreDialog3.addEventListener('click', function() {
+  dialog.classList.add('dialog--aberto');
+});
+
+function fechandoDialog() {
+  document.activeElement.blur();
+  dialog.classList.remove('dialog--aberto');     
+}
+
+// Listeners
+document.querySelector('.dialog-fechar').addEventListener('click', fechandoDialog);
+
+
+
+// Abrir dialog 4
+btnAbreDialog4.addEventListener('click', function() {
+  dialog.classList.add('dialog--aberto');
+});
+
+function fechandoDialog() {
+  document.activeElement.blur();
+  dialog.classList.remove('dialog--aberto');     
+}
+
+// Listeners
+document.querySelector('.dialog-fechar').addEventListener('click', fechandoDialog);
+
+
+
+// Abrir dialog 5
+btnAbreDialog5.addEventListener('click', function() {
+  dialog.classList.add('dialog--aberto');
+});
+
+function fechandoDialog() {
+  document.activeElement.blur();
+  dialog.classList.remove('dialog--aberto');     
+}
+
+// Listeners
+document.querySelector('.dialog-fechar').addEventListener('click', fechandoDialog);
+
+
+
+// Abrir dialog 6
+btnAbreDialog6.addEventListener('click', function() {
+  dialog.classList.add('dialog--aberto');
+});
+
+function fechandoDialog() {
+  document.activeElement.blur();
+  dialog.classList.remove('dialog--aberto');     
+}
+
+// Listeners
+document.querySelector('.dialog-fechar').addEventListener('click', fechandoDialog);
